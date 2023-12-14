ae33e7aa40d29be72db5e01cde804cba44ad3be8
Alejandro Piñango <alejandropinango@gmail.com>
2023-12-14 12:10:33 -0300

    style: agregar bootstrap y campo perfil a las vistas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [
                                                         :first_name,
                                                         :last_name,
-                                                        :role
+                                                        :role,
+                                                        :photo
+
                                                        ])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [
                                                                 :first_name,
                                                                 :last_name,
-                                                                :role
+                                                                :role,
+                                                                :photo
                                                               ])
 
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   # Enum de roles
   enum role: [:user, :admin]
 
+    # Defino una foto al usuario
+    has_one_attached :photo
+
 end
